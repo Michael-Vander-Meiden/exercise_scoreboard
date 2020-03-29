4d6af3b4891c41a32e1f0333c51c0bbaa5bef0d3
Michael-Vander-Meiden <michaelvandermeiden@gmail.com>
2020-03-29 13:59:15 -0700

    fixed url linkages

--- scoreboard/urls.py
+++ scoreboard/urls.py
@@ -6,11 +6,11 @@ app_name = 'scoreboard'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<str:competition_name>/', views.competition, name='competition'),
-    path('<str:participant_name>/detail', views.detail, name='detail'),
-    path('<str:participant_name>/log_pushups', views.log_pushups, name='log_pushups'),
-    path('<str:participant_name>/log_situps', views.log_situps, name='log_situps'),
-    path('<str:participant_name>/log_squats', views.log_squats, name='log_squats'),
-    path('<str:participant_name>/log_pullups', views.log_pullups, name='log_pullups'),
-    path('<str:participant_name>/log_dips', views.log_dips, name='log_dips'),
+    path('<str:competition_name>/<str:participant_name>/detail', views.detail, name='detail'),
+    path('<str:competition_name>/<str:participant_name>/log_pushups', views.log_pushups, name='log_pushups'),
+    path('<str:competition_name>/<str:participant_name>/log_situps', views.log_situps, name='log_situps'),
+    path('<str:competition_name>/<str:participant_name>/log_squats', views.log_squats, name='log_squats'),
+    path('<str:competition_name>/<str:participant_name>/log_pullups', views.log_pullups, name='log_pullups'),
+    path('<str:competition_name>/<str:participant_name>/log_dips', views.log_dips, name='log_dips'),
 
 ]
--- scoreboard/views.py
+++ scoreboard/views.py
@@ -10,55 +10,55 @@ def index(request):
     			'top_participant': latest_scores_list[0]}
     return render(request, 'scoreboard/index.html', context)
 
-def competition(request, competition_name):
+def competition(request, competition_name, ):
     competition = get_object_or_404(Competition, competition_name=competition_name)
     latest_scores_list = competition.participant_set.all().order_by('-total_score')
     context = {'latest_scores_list': latest_scores_list,
     			'top_participant': latest_scores_list[0]}
     return render(request, 'scoreboard/index.html', context)
 
-def detail(request, participant_name):
+def detail(request, competition_name, participant_name):
 	participant = get_object_or_404(Participant, participant_name=participant_name)
 	context = {'participant': participant}
 	return render(request, 'scoreboard/detail.html', context)
 
 
-def log_pushups(request, participant_name):
+def log_pushups(request, competition_name, participant_name):
 	participant = get_object_or_404(Participant, participant_name=participant_name)
 	quantity = int(request.POST['quantity'])
 	participant.score += quantity
 	participant.save()
 	context = {'participant': participant}
-	return HttpResponseRedirect(reverse('scoreboard:index'))
+	return HttpResponseRedirect(reverse('scoreboard:competition', args=[competition_name]))
 
-def log_situps(request, participant_name):
+def log_situps(request, competition_name, participant_name):
 	participant = get_object_or_404(Participant, participant_name=participant_name)
 	quantity = int(request.POST['quantity'])
 	participant.situps_count += quantity
 	participant.save()
 	context = {'participant': participant}
-	return HttpResponseRedirect(reverse('scoreboard:index'))
+	return HttpResponseRedirect(reverse('scoreboard:competition', args=[competition_name]))
 
-def log_squats(request, participant_name):
+def log_squats(request, competition_name, participant_name):
 	participant = get_object_or_404(Participant, participant_name=participant_name)
 	quantity = int(request.POST['quantity'])
 	participant.squats_count += quantity
 	participant.save()
 	context = {'participant': participant}
-	return HttpResponseRedirect(reverse('scoreboard:index'))
+	return HttpResponseRedirect(reverse('scoreboard:competition', args=[competition_name]))
 
-def log_pullups(request, participant_name):
+def log_pullups(request, competition_name, participant_name):
 	participant = get_object_or_404(Participant, participant_name=participant_name)
 	quantity = int(request.POST['quantity'])
 	participant.pullups_count += quantity
 	participant.save()
 	context = {'participant': participant}
-	return HttpResponseRedirect(reverse('scoreboard:index'))
+	return HttpResponseRedirect(reverse('scoreboard:competition', args=[competition_name]))
 
-def log_dips(request, participant_name):
+def log_dips(request, competition_name, participant_name):
 	participant = get_object_or_404(Participant, participant_name=participant_name)
 	quantity = int(request.POST['quantity'])
 	participant.dips_count += quantity
 	participant.save()
 	context = {'participant': participant}
-	return HttpResponseRedirect(reverse('scoreboard:index'))
+	return HttpResponseRedirect(reverse('scoreboard:competition', args=[competition_name]))
